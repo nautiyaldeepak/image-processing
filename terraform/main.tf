13dffcfaf3cd1c9741bda4fac1151be98e9de36f
Deepak Nautiyal <deepaknautiyal@Deepaks-MBP.fritz.box>
2024-06-09 16:58:36 +0200

    adding datasync resources | terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,5 @@
+data "aws_caller_identity" "current" {}
+
 resource "random_string" "suffix" {
   length  = 6
   special = false
@@ -102,10 +104,10 @@ resource "aws_efs_mount_target" "efs_mount_target" {
   for_each = { for idx, subnet_id in var.subnet_ids : idx => subnet_id }
   file_system_id  = aws_efs_file_system.efs_file_system.id
   subnet_id       = each.value
-  security_groups = [aws_security_group.efs.id]
+  security_groups = [aws_security_group.efs_security_group.id]
 }
 
-resource "aws_security_group" "efs" {
+resource "aws_security_group" "efs_security_group" {
   name   = "allow_efs"
   vpc_id = var.vpc_id
 
@@ -123,6 +125,89 @@ resource "aws_security_group" "efs" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  tags = {
+    Environment = "${var.identifier}"
+  }
+}
+
+resource "aws_iam_role" "datasync_s3_role" {
+  name = "${var.identifier}-datasync-s3-role"
+
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": "datasync.amazonaws.com"
+        },
+        "Effect": "Allow",
+        "Sid": ""
+      }
+    ]
+  })
+
+  inline_policy {
+    name   = "${var.identifier}-s3-access-datasync-policy"
+    policy = jsonencode({
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Effect": "Allow",
+          "Action": [
+            "s3:ListBucket",
+            "s3:GetObject",
+            "s3:GetObjectVersion",
+            "s3:GetBucketLocation"
+          ],
+          "Resource": [
+            aws_s3_bucket.origin_bucket.arn,
+            aws_s3_bucket.transform_bucket.arn,
+            "${aws_s3_bucket.origin_bucket.arn}/*",
+            "${aws_s3_bucket.transform_bucket.arn}/*"
+          ]
+        }
+      ]
+    })
+  }
+  tags = {
+    Environment = "${var.identifier}"
+  }
+}
+
+resource "aws_datasync_location_s3" "datasync_s3_origin_location" {
+  s3_bucket_arn = aws_s3_bucket.origin_bucket.arn
+  subdirectory  = "/"  # Optional: If you want to specify a subdirectory within the S3 bucket
+  s3_config {
+    bucket_access_role_arn = aws_iam_role.datasync_s3_role.arn
+  }
+  tags = {
+    Environment = "${var.identifier}"
+  }
+}
+
+resource "aws_datasync_location_s3" "datasync_s3_transform_location" {
+  s3_bucket_arn = aws_s3_bucket.transform_bucket.arn
+  subdirectory  = "/"  # Optional: If you want to specify a subdirectory within the S3 bucket
+  s3_config {
+    bucket_access_role_arn = aws_iam_role.datasync_s3_role.arn
+  }
+  tags = {
+    Environment = "${var.identifier}"
+  }
+}
+
+resource "aws_datasync_location_efs" "datasync_efs_location" {
+  ec2_config {
+    security_group_arns = [aws_security_group.efs_security_group.arn]
+    subnet_arn          = "arn:aws:ec2:${var.aws_region}:${data.aws_caller_identity.current.account_id}:subnet/${var.subnet_ids[0]}"
+  }
+  efs_file_system_arn = aws_efs_file_system.efs_file_system.arn
+  subdirectory        = "/"  # Optional: If you want to specify a subdirectory within the EFS file system
+  depends_on = [
+    aws_efs_file_system.efs_file_system,
+    aws_efs_mount_target.efs_mount_target
+  ]
   tags = {
     Environment = "${var.identifier}"
   }
