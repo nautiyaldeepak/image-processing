d6f0886f4f38b322c6c2680748244cf3abbf85e6
Deepak Nautiyal <deepaknautiyal@Deepaks-MBP.fritz.box>
2024-06-10 00:16:04 +0200

    removing directory

--- lambda_functions_scripts/s3_trigger/s3_trigger.py
+++
@@ -1,8 +0,0 @@
-import boto3
-
-def lambda_handler(event, context):
-    # Extract bucket name and object key from the S3 event
-    bucket_name = event['detail']['bucket']['name']
-    object_key = event['detail']['object']['key']
-    print("bucketName: " + bucket_name)
-    print("ObjectName: " + object_key)
