701eedd0137cbdefc4a948e3e15d991afb2a40ed
Deepak Nautiyal <deepaknautiyal@Deepaks-MBP.fritz.box>
2024-06-10 02:07:47 +0200

    updating variables

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -25,14 +25,14 @@ variable "identifier" {
 variable "subnet_ids" {
   description = "A list of subnet IDs where the EFS mount targets will be created."
   type        = list(string)
-  default     = ["subnet-0a4633ceb176cfd9d", "subnet-0ef89c8644f17f2ed", "subnet-0c756ca41c97718ea"]  # Add default as an empty list
+  default     = []  # Add default as an empty list
 #   default     = ["subnet-0a4", "subnet-0ef", "subnet-0c756c"]
 }
 
 variable "vpc_id" {
   description = "The VPC ID where the EFS file system will be created."
   type        = string
-  default     = "vpc-0ffa5e60cc0208c47"  # Add default as an empty string
+  default     = ""  # Add default as an empty string
 }
 
 variable "vpc_cidr_block" {
